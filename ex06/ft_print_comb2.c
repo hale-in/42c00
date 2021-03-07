137b2dd4fd04871e0e5e7778ade9131a4ad6ba0a
Taehee Kim <taeheeki@c8r1s6.42seoul.kr>
2021-03-07 10:46:43 +0900

    c00

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taeheeki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/25 15:15:01 by taeheeki          #+#    #+#             */
+/*   Updated: 2021/03/07 10:22:13 by taeheeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(int num)
+{
+	char ten;
+	char one;
+
+	ten = num / 10 + '0';
+	one = num % 10 + '0';
+	write(1, &ten, 1);
+	write(1, &one, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a);
+			write(1, " ", 1);
+			ft_putchar(b);
+			if (!(a == 98 && b == 99))
+			{
+				write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
+}
